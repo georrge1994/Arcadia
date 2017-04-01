20f7373b2b6ee68645ae6c24558b040356ec7c40
georrge1994 <Zhore-Chebotarev@mail.ru>
2017-04-01 23:55:17 +0300

    add file MAksim

---
+++ ArcadiaWebProject/src/main/java/support/Collection.java
@@ -0,0 +1,13 @@
+package support;
+
+import java.util.ArrayList;
+
+public class Collection {
+
+    public ArrayList<ArrayList<String>> outer = new ArrayList<ArrayList<String>>();
+
+    public void addArrayList(ArrayList<String> arrayList)
+    {
+        outer.add(arrayList);
+    }
+}
