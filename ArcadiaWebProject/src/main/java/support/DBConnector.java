c20456a5a0ca4ed33d4bb327267972af780fea32
Sublimee <aquariusborelias@gmail.com>
2017-04-02 18:32:54 +0300

    DBC connector update

--- ArcadiaWebProject/src/main/java/support/DBConnector.java
+++ ArcadiaWebProject/src/main/java/support/DBConnector.java
@@ -7,33 +7,37 @@ import com.google.firebase.database.*;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
-public class DBConnector {
+/**
+ * Created by Sublimee on 15.03.2017.
+ */
+public class DBConnector  {
 
-    public synchronized static void init() throws FileNotFoundException {
-        FileInputStream serviceAccount = new FileInputStream("C:\\Projects\\arcadia_test_projects\\lesson2\\coopcopy-5dc9f-firebase-adminsdk-zgprh-9bab625579.json");
+
+    public static void init() throws FileNotFoundException
+    {
+        FileInputStream serviceAccount = new FileInputStream("C:\\IntelliJIDEA2016.3.5(64)Project\\Arcadia\\ArcadiaWebProject\\coopcopy-5dc9f-firebase-adminsdk-zgprh-9bab625579.json");
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredential(FirebaseCredentials.fromCertificate(serviceAccount))
                 .setDatabaseUrl("https://coopcopy-5dc9f.firebaseio.com/")
                 .build();
-        boolean hasBeenInitialized = false;
+        boolean hasBeenInitialized=false;
         List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
-        for (FirebaseApp app : firebaseApps) {
-            if (app.getName().equals("[DEFAULT]")) {
-                hasBeenInitialized = true;
+        for(FirebaseApp app : firebaseApps){
+            if(app.getName().equals("[DEFAULT]")){
+                hasBeenInitialized=true;
             }
         }
 
-        if (!hasBeenInitialized) {
+        if(!hasBeenInitialized) {
             FirebaseApp.initializeApp(options);
         }
     }
-
-    public void getSignedUsersBetweenDates(final Collection collection, long date1 , long date2) {
-
+	
+	    public void getSignedUsersBetweenDates(final Collection collection, long date1 , long date2) {
 
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference ref = database.getReference("/users");
@@ -66,10 +70,7 @@ public class DBConnector {
             }
         });
     }
+
+
+
 }
-
-
-
-
-
-
--- ArcadiaWebProject/src/main/webapp/js/report-designer.js
+++ ArcadiaWebProject/src/main/webapp/js/report-designer.js
@@ -15,20 +15,15 @@ $('#design_form input').on('change', function() {
 function DisableUnnecessary(){
     select_report = $('input[name=select-report]:checked', '#design_form').val(); // get class name checked report
 
-    $('#design_form :input').prop('disabled', true);    // disable all
+     $('#design_form :input').prop('disabled', true);    // disable all
 
     $('.' + select_report).prop('disabled', false);        // enable a needed report
     $('.special-radio').prop('disabled', false);        // enable radio-buttons
     $('.sendsubmit').prop('disabled',false);            // enable send-button
+    $('.type_report').prop('disabled',false);            // enable type_report
 }
 
-function createPostData(){
-    var index_report = select_report.replace('report','');
-    var str = 'type_report=' + index_report + '&'+ $('.' + select_report).serialize();
-    return str;
-}
-
-
+/*
 $(document).ready(function() {
     var form = $('#design_form');       // contact form
     var submit = $('#submit_xml');      // submit button
@@ -45,8 +40,44 @@ $(document).ready(function() {
             beforeSend: function() {
                 console.log("wait..");
             },
-            success: function(data) {
-                console.log("success");
+            success: function(response, status, xhr) {
+                // check for a filename
+                var filename = "";
+                var disposition = xhr.getResponseHeader('Content-Disposition');
+                if (disposition && disposition.indexOf('attachment') !== -1) {
+                    var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+                    var matches = filenameRegex.exec(disposition);
+                    if (matches != null && matches[1]) filename = matches[1].replace(/['"]/g, '');
+                }
+
+                var type = xhr.getResponseHeader('Content-Type');
+                var blob = new Blob([response], { type: type });
+
+                if (typeof window.navigator.msSaveBlob !== 'undefined') {
+                    // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
+                    window.navigator.msSaveBlob(blob, filename);
+                } else {
+                    var URL = window.URL || window.webkitURL;
+                    var downloadUrl = URL.createObjectURL(blob);
+
+                    if (filename) {
+                        // use HTML5 a[download] attribute to specify filename
+                        var a = document.createElement("a");
+                        // safari doesn't support this yet
+                        if (typeof a.download === 'undefined') {
+                            window.location = downloadUrl;
+                        } else {
+                            a.href = downloadUrl;
+                            a.download = filename;
+                            document.body.appendChild(a);
+                            a.click();
+                        }
+                    } else {
+                        window.location = downloadUrl;
+                    }
+
+                    setTimeout(function () { URL.revokeObjectURL(downloadUrl); }, 100); // cleanup
+                }
             },
             error: function(e) {
                 console.log(e)
@@ -55,3 +86,4 @@ $(document).ready(function() {
     });
 });
 
+*/
