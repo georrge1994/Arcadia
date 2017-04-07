f5697d94d16cf3e948b8d6b3be9b4145b767329f
Sublimee <aquariusborelias@gmail.com>
2017-04-07 23:32:33 +0300

    test

--- ArcadiaWebProject/src/main/java/support/User.java
+++ ArcadiaWebProject/src/main/java/support/User.java
@@ -1,18 +1,51 @@
 package support;
 
-public class User implements Comparable<User>{
-    String email = "";
-    String name = "";
-    Long rating = 0L;
+import java.util.HashMap;
+import java.util.Map;
 
-    public User (String email, String name, Long rating){
-        this.email = email;
-        this.name = name;
-        this.rating = rating;
+/**
+ * Created by Sublimee on 15.03.2017.
+ */
+
+// Информация хранится непосредственно в таблице "users"
+public class User extends LocalUser {
+
+    String userKey = "";
+    Map<Group, User> hashMap = new HashMap<Group, User>();
+
+    public User (String key, String email, String name, String role){
+
+        this.userKey = key;
+        this.email = email;// глобальный email пользователя
+        this.name = name;  // глобальное имя пользователя
+        this.role = role;  // глобальная роль пользователя
+
+
     }
 
-    @Override
-    public int compareTo(User anotherUser) {
-        return anotherUser.rating.compareTo(this.rating);
+    public String getMail(){
+        return this.email;
     }
+
+    public String getKey(){
+        return this.userKey;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getRole(){
+        return this.role;
+    }
+
+    //пока нет даты
+    public String getRegistrationDate() {
+        String str = "Registration Date";
+        return str;
+    }
+    //Vector groups = new Vector();
+    //String avatarUrl = "";
+
+
 }
